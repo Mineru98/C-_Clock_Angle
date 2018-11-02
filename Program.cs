d3a54b6348df303a26427f71bc6c455a5a89b2c4
mineru <rmstjr1030@naver.com>
2018-11-02 13:59:53 +0900

    Main Source Code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Clock
+{
+    class Program
+    {
+        static double Clock(int hour, int min)
+        {
+            double result;
+            result = (hour * 30 + min * 1.2) - (min * 6);
+            if (result < 0)
+                result *= -1;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int hour, minute;
+            Console.Write("How many hours?");
+            hour = int.Parse(Console.ReadLine());
+            Console.Write("How many minutes?");
+            minute = int.Parse(Console.ReadLine());
+            Console.WriteLine("It is {2} degrees at {0}:{1}.", hour, minute, Clock(hour, minute));
+        }
+    }
+}
